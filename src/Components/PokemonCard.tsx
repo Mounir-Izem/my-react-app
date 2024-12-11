c94cc4cbf4d0851b47eb2c3780a3481831323e71
Mounir Izem <izem.mounir@gmail.com>
2024-12-12 00:19:20 +0100

    Utilisation de la méthode ternaire pour affiché les composants

--- src/Components/PokemonCard.tsx
+++ src/Components/PokemonCard.tsx
@@ -1,9 +1,29 @@
 export default function PokemonCard() {
+
+const pokemon = pokemonList[0];
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>Bulbasaur</figcaption>
+            {
+                pokemon.imgSrc && pokemon.name ?(
+                    <div>
+                        <img src ={pokemon.imgSrc} alt={"Photo d'un pokémon"}/> <p>{pokemon.name}</p>
+                    </div>
+                    
+                ) : (
+                    <p>???</p>
+                )
+            }
         </figure>
     )
 }
 
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
