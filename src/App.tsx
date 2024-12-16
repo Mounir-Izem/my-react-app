8f532bbad3af8f59ffc08f474d15030653feb97c
Mounir Izem <izem.mounir@gmail.com>
2024-12-16 17:22:04 +0100

    Ajout de 2 boutons avec comme fonction, changement de pokemons au click

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
+import { useState } from "react";
 import PokemonCard from "./Components/PokemonCard";
 
-
 function App() {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-    },
-    {
-      name: "mew"
-    },
-  ];
-  
-  return (
-    <div>
-      <PokemonCard pokemon ={pokemonList[0]}/>
-    </div>
-  )
- 
+	const pokemonList = [
+		{
+			name: "bulbasaur",
+			imgSrc:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+		},
+		{
+			name: "charmander",
+			imgSrc:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+		},
+		{
+			name: "squirtle",
+			imgSrc:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+		},
+		{
+			name: "pikachu",
+			imgSrc:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+		},
+		{
+			name: "mew",
+		},
+	];
+
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+
+	const pokemonSuivant = () => {
+		if (pokemonIndex < pokemonList.length - 1) {
+			setPokemonIndex(pokemonIndex + 1);
+		}
+	};
+	const pokemonPrecedent = () => {
+		pokemonIndex === 0 ? setPokemonIndex(0) : setPokemonIndex(pokemonIndex - 1);
+	};
+
+	return (
+		<div>
+			<div>
+				<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+			</div>
+			<div>
+				<button type="button" onClick={pokemonPrecedent}>
+					Précedant
+				</button>
+				<button type="button" onClick={pokemonSuivant}>
+					Suivant
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/Components/PokemonCard.tsx
+++ src/Components/PokemonCard.tsx
@@ -1,27 +1,21 @@
 interface PokeType {
-    pokemon: {
-    name: string;
-    imgSrc?: string;
-    }
-};
-
-
-
-export default function PokemonCard({pokemon}: PokeType) {
-
-    return (
-        <figure>
-            {
-                pokemon.imgSrc && pokemon.name ?(
-                    <div>
-                        <img src ={pokemon.imgSrc} alt={"Photo d'un pokémon"}/> 
-                        <p>{pokemon.name}</p>
-                    </div> 
-                ) : (
-                    <p>???</p>
-                )
-            }
-        </figure>
-    )
+	pokemon: {
+		name: string;
+		imgSrc?: string;
+	};
 }
 
+export default function PokemonCard({ pokemon }: PokeType) {
+	return (
+		<figure>
+			{pokemon.imgSrc && pokemon.name ? (
+				<div>
+					<img src={pokemon.imgSrc} alt={pokemon.name} />
+					<p>{pokemon.name}</p>
+				</div>
+			) : (
+				<p>???</p>
+			)}
+		</figure>
+	);
+}
